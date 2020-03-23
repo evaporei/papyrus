42040a193caf5f4519a4d6987ee0e913bb5e84e6
Otávio Pace <otaviopp8@gmail.com>
2020-03-23 00:41:11 -0300

    main: create simple SubCommands structure with structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(subcommand)]
+    sub_commands: SubCommands,
+}
+
+#[derive(StructOpt, Debug)]
+enum SubCommands {
+    HashObject { object: PathBuf },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
 }
